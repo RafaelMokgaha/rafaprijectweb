fcb0e868cdafb5ee92e67dd816409257fbb2e1d8
Thabang Mokgaha <thabangmoo5@gmail.com>
2025-10-19 16:40:36 +0000

    remove ads scene code on my site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Oswald:wght@400;700&display=swap" rel="stylesheet" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6687235953068951"
-     crossOrigin="anonymous"></script>
       </head>
       <body>
         <FirebaseClientProvider>
