27c1dba1f74bb1f36f3a004bdcef1203a1304a96
Thabang Mokgaha <thabangmoo5@gmail.com>
2025-10-19 13:27:01 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,6 @@
-
 export const firebaseConfig = {
   "projectId": "studio-5428370737-6a4fc",
   "appId": "1:279593063955:web:aff732ba6d1ce4909691e6",
-  "storageBucket": "studio-5428370737-6a4fc.appspot.com",
   "apiKey": "AIzaSyByozLnK5odpc9ZjFm3VzvY_htqE0F_9Co",
   "authDomain": "studio-5428370737-6a4fc.firebaseapp.com",
   "measurementId": "",
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,35 +1,42 @@
-
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
-let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
-
-// This function ensures Firebase is initialized only once.
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    firebaseApp = initializeApp(firebaseConfig);
-  } else {
-    firebaseApp = getApp();
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
-  auth = getAuth(firebaseApp);
-  firestore = getFirestore(firebaseApp);
-  
-  return { firebaseApp, auth, firestore };
+
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
-  const firestore = getFirestore(firebaseApp);
-  
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore,
+    firestore: getFirestore(firebaseApp)
   };
 }
 
