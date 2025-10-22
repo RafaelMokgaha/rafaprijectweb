2dfb913aefcc8fa5a55aa67955f146b59798c01a
Thabang Mokgaha <thabangmoo5@gmail.com>
2025-10-22 08:03:50 +0000

    put this picture as the background

--- next.config.js
+++ next.config.js
@@ -46,6 +46,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
